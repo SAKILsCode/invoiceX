2a9d815766ddfcb13982f91275b8cc4068a9bbf1
MD Sakil Hasan <mdsakilhasan833@gmail.com>
2023-03-06 14:48:25 +0600

    routes init 🥚

---
+++ src/routes/index.js
@@ -0,0 +1,32 @@
+import Home from "../pages/Home";
+import Dashboard from "../pages/Dashboard";
+import Error from "../pages/Error";
+
+
+export const routes = [
+  {
+    path: '/',
+    isProtected: false,
+    component: Home,
+  },
+  {
+    path: '/dashboard',
+    isProtected: true,
+    component: Dashboard,
+  },
+  // {
+  //   path: '/auth/login',
+  //   isProtected: false,
+  //   component: SignIn,
+  // },
+  // {
+  //   path: '/auth/register',
+  //   isProtected: false,
+  //   component: SignUp,
+  // },
+  {
+    path: '*',
+    isProtected: false,
+    component: Error,
+  },
+];
